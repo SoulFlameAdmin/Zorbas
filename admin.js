73c425c54bbb8270ccef63e88ec19ec8b7f7832c
SoulFlameAdmin <soulflame.mitko@gmail.com>
2026-07-30 21:56:15 +0300

    Deploy automatic BAR and KITCHEN routing to production

--- admin.js
+++ admin.js
@@ -1,8 +1,8 @@
 (() => {
   const files = [
     '/admin-core.js',
-    '/admin-order.js?v=20260730-routing2',
-    '/admin-auto-routing.js?v=20260730-routing2',
+    '/admin-order.js?v=20260730-routing3',
+    '/admin-auto-routing.js?v=20260730-routing3',
     '/admin-waiter.js?v=20260730-waiter1',
     '/admin-waiter-hook.js?v=20260730-waiter1',
     '/admin-waiter-v2.js?v=20260730-waiter2',
