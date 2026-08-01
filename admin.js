06379a54a3746111ca64920cce07879f34bd7724
SoulFlameAdmin <soulflame.mitko@gmail.com>
2026-08-01 13:06:30 +0300

    Load approved waiter home design v2

--- admin.js
+++ admin.js
@@ -20,7 +20,7 @@
     '/admin-stage3.js?v=20260731-stage3c',
     '/admin-stage3-note-checks.js?v=20260731-notechecks1',
     '/admin-note-input-stability.js?v=20260801-note2',
-    '/admin-waiter-home-v1.js?v=20260801-home1'
+    '/admin-waiter-home-v1.js?v=20260801-home2'
   ];
 
   const load = src => new Promise((resolve, reject) => {
