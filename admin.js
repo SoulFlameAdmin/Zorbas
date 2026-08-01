f85146d1f5beb40e54bf917a54c2cecfee652e5f
SoulFlameAdmin <soulflame.mitko@gmail.com>
2026-08-01 16:02:28 +0300

    Load bill visibility rule for fully issued guests

--- admin.js
+++ admin.js
@@ -21,7 +21,7 @@
     '/admin-stage3-note-checks.js?v=20260731-notechecks1',
     '/admin-note-input-stability.js?v=20260801-note2',
     '/admin-waiter-home-v1.js?v=20260801-home2',
-    '/admin-waiter-note-final.js?v=20260801-note3',
+    '/admin-waiter-note-final.js?v=20260801-note4',
     '/admin-manager-active-final.js?v=20260801-manager-active1',
     '/admin-manager-stability-v2.js?v=20260801-manager-stable2',
     '/admin-waiter-receipts-final.js?v=20260801-waiter-receipts2'
